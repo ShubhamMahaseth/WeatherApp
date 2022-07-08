ae52b9f414ca36dbb39d2b1b564c0953fac1aac7
Shubham Mahaseth <mahasethshubham@gmail.com>
2022-07-08 22:03:06 +0530

    added functionality

--- App.js
+++ App.js
@@ -1,111 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {useState, useCallback} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  TextInput,
+  ActivityIndicator,
 } from 'react-native';
+import Axios from 'axios';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
+  const [change, setChange] = useState(null);
+  const [dataWeather, setData] = useState([]);
+  const [indicator, setIndicator] = useState(false);
+  const api = {
+    baseUrl:
+      'https://api.openweathermap.org/data/2.5/weather?q=Pune&units=metric&appid=',
+    key: '2819e53c1fac69523ea356741003e23b',
+  };
+  console.log(change);
+  const enter = useCallback(() => {
+    setChange('');
+    setIndicator(true);
+    Axios({
+      method: 'get',
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${change}&units=metric&appid=${api.key}`,
+    })
+      .then(res => {
+        setData(res);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIndicator(false);
+      });
+  }, [api.key, change]);
+  const image = {uri: 'https://www.fnordware.com/superpng/pnggrad16rgb.png'};
+  console.log(dataWeather.data);
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={image}
+        resizeMode="cover"
+        style={styles.imageWrapper}>
+        <TextInput
+          placeholder="Enter city name"
+          onChangeText={text => {
+            setChange(text);
+          }}
+          value={change}
+          autoFocus={true}
+          placeholderTextColor="grey"
+          style={styles.textWrapper}
+          textAlign={'left'}
+          selectionColor={'grey'}
+          onEndEditing={enter}
+        />
+        {indicator ? <ActivityIndicator size="large" color="white" /> : null}
+        {dataWeather ? (
+          <View>
+            <Text>{dataWeather.data ? dataWeather.data.name : null}</Text>
+          </View>
+        ) : null}
+      </ImageBackground>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {flex: 1},
+  imageWrapper: {flex: 1},
+  textWrapper: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'gray',
+    backgroundColor: 'white',
+    margin: 16,
+    color: 'black',
+    elevation: 5,
   },
 });
 
