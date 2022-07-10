b3b51b18f117bb01181da3c0f7350d6d3f44241a
Shubham Mahaseth <mahasethshubham@gmail.com>
2022-07-10 23:41:09 +0530

    added background screen

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,13 +1,87 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useCallback} from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  TextInput,
+  ActivityIndicator,
+} from 'react-native';
 import {styles} from './style';
-import Moon from '../../../assets/moon.svg';
+import moment from 'moment';
+import Axios from 'axios';
 
 const Screen = () => {
+  const [change, setChange] = useState(null);
+  const [dataWeather, setData] = useState([]);
+  const [indicator, setIndicator] = useState(false);
+  const api = {
+    baseUrl:
+      'https://api.openweathermap.org/data/2.5/weather?q=Pune&units=metric&appid=',
+    key: '2819e53c1fac69523ea356741003e23b',
+  };
+
+  const enter = useCallback(() => {
+    setChange('');
+    setIndicator(true);
+    Axios({
+      method: 'get',
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${change}&units=metric&appid=${api.key}`,
+    })
+      .then(res => {
+        setData(res);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        setIndicator(false);
+      });
+  }, [api.key, change]);
+
+  const weatherImage = function () {
+    if (dataWeather.data?.weather[0].main == 'Clouds') {
+      return require('../../../assets/sunny.jpg');
+    } else if (dataWeather.data?.weather[0].main == 'Haze') {
+      return require('../../../assets/cloudy.jpeg');
+    } else if (dataWeather.data?.weather[0].main == 'Rain') {
+      return require('../../../assets/rainy.jpg');
+    } else if (dataWeather.data?.weather[0].main == 'Clear') {
+      return require('../../../assets/sunny.jpg');
+    } else if (dataWeather.data?.weather[0].main == 'scattered clouds') {
+      return require('../../../assets/scatterd.jpg');
+    } else {
+      return require('../../../assets/bg3.jpeg');
+    }
+  };
+
+  console.log(dataWeather.data?.weather[0].main);
+  console.log('--------', moment(new Date()).format('hh:mm:ss'));
   return (
-    <View>
-      <Moon width={26} height={26} fill="#fff" />
-      <Text style={styles.textWrapper}>Hello</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={weatherImage()}
+        resizeMode="cover"
+        style={styles.imageWrapper}>
+        <TextInput
+          placeholder="Enter city name"
+          onChangeText={text => {
+            setChange(text);
+          }}
+          value={change}
+          autoFocus={true}
+          placeholderTextColor="grey"
+          style={styles.textWrapper}
+          textAlign={'left'}
+          selectionColor={'grey'}
+          onEndEditing={enter}
+        />
+        {indicator ? <ActivityIndicator size="large" color="white" /> : null}
+        {dataWeather ? (
+          <View>
+            <Text>{dataWeather.data ? dataWeather.data.name : null}</Text>
+          </View>
+        ) : null}
+      </ImageBackground>
     </View>
   );
 };
--- src/components/Screen/style.js
+++ src/components/Screen/style.js
@@ -1,5 +1,19 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 
 export const styles = StyleSheet.create({
-  textWrapper: {color: 'red', fontFamily: 'Lato-Bold'},
+  container: {flex: 1},
+  imageWrapper: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  textWrapper: {
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: 'gray',
+    backgroundColor: 'white',
+    margin: 16,
+    color: 'black',
+    elevation: 5,
+  },
 });
